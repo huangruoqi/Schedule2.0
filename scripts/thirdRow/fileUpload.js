e7375c0ccdc66092a8006352dd9e246ce4eec57c
Ruoqi Huang <huangruoqi374@gmail.com>
2021-01-04 19:06:17 -0800

    file upload done and done

--- scripts/thirdRow/fileUpload.js
+++ scripts/thirdRow/fileUpload.js
@@ -1,74 +1,59 @@
 function URLAndName(url, name) {
     this.url = url;
     this.name = name;
+    this.equals = n=> {
+        return n.name.localeCompare(this.name)==0
+    }
 }
-var uploadedFileList;
+
+var uploadedFileList = [];
 var isDeleting = false;
+const d = document.getElementsByClassName("dd")[0];
+d.addEventListener('click', element => {
+    isDeleting = !isDeleting;
+    setDownloadOrDelete();
+    setupFileList()
+})
+const u = document.getElementsByClassName("uu")[0];
+u.addEventListener('click', element => {
+    document.getElementById('uploadInput').click();
+})
+
+
+
 function changeFileUpload() {
     const ac = document.getElementsByClassName('fileUpload')[0];
-    removeAllChildNodes(ac);
     ac.style.height = convertHeight(14);
-    const fileList = document.createElement('div');
+    getUploadedFiles()
+    setDownloadOrDelete();
+    setUpload();
 
-    getUploadedFiles().then(element => {
-        uploadFilesList = element;
-        setupFileList(fileList);
-
-        ac.appendChild(fileList);
-        const DD = document.createElement('div');
-        const DDcontainer = document.createElement('div');
-        DDcontainer.className = "dd-container";
-        DD.addEventListener('click', element => {
-            isDeleting = !isDeleting;
-            removeAllChildNodes(DD);
-            setDownloadOrDelete(DD);
-        })
-        setDownloadOrDelete(DD);
-        DDcontainer.appendChild(DD);
-        ac.appendChild(DDcontainer);
-
-        const UU = document.createElement('div');
-        const UUcontainer = document.createElement('div');
-        UUcontainer.className = "uu-container";
-
-        UU.addEventListener('click', element => {
-            document.getElementById('uploadInput').click();
-        })
-
-        UU.style.fontSize = convertHeight(1.3)
-        const text = document.createElement('div');
-        text.className = "uu-text";
-        text.appendChild(document.createTextNode("Upload"));
-        UU.appendChild(text);
-        UU.className = "uu";
-        const upload = document.getElementById("uploadInput");
-        upload.addEventListener('change', element => {
-            uploadFiles(upload.files).then(e => {
-                changeFileUpload();
-            });
-        })
-        UUcontainer.appendChild(UU);
-        ac.appendChild(UUcontainer);
-    })
 }
 
-function setupFileList(fileList) {
+function setupFileList() {
+    const fileList = document.getElementsByClassName("uploadedFiles")[0];
     removeAllChildNodes(fileList);
-    fileList.className = "uploadedFiles";
-    fileList.style.fontSize = convertHeight(1.1);
-    for (let i = 0; i < uploadFilesList.length; i++) {
+    fileList.style.fontSize = convertHeight(1);
+    for (let i = 0; i < uploadedFileList.length; i++) {
         if (i == 12) { break }
         const temp = document.createElement('div');
         temp.className = "upload-container"
         temp.addEventListener("click", e => {
-            deleteOrDownloadFile(uploadFilesList[i]);
+            deleteOrDownloadFile(uploadedFileList[i]);
         })
-        let s = uploadFilesList[i].name;
-        if (s.length > 13) {
-            s = s.substring(0, 10) + "...";
+        let s = uploadedFileList[i].name;
+        if (s.length > 15) {
+            s = s.substring(0, 12) + "...";
+        }
+
+        if (isDeleting) {
+            temp.classList.remove("HoverClassRed", "HoverClassGreen");
+            temp.classList.add('HoverClassRed')
+        } else {
+            temp.classList.remove("HoverClassRed", "HoverClassGreen");
+            temp.classList.add('HoverClassGreen')
         }
         const inside = document.createElement('a');
-
         inside.appendChild(document.createTextNode(s)); // remove ".pdf"
 
         temp.appendChild(inside);
@@ -76,51 +61,92 @@ function setupFileList(fileList) {
     }
 }
 
-function setDownloadOrDelete(d) {
+function AddSingleFileToList(file) {
+    const fileList = document.getElementsByClassName("uploadedFiles")[0];
+    fileList.style.fontSize = convertHeight(1);
+    if (uploadedFileList.length > 12) return
+
+    const temp = document.createElement('div');
+    temp.className = "upload-container"
+    temp.addEventListener("click", e => {
+        deleteOrDownloadFile(file);
+    })
+    let s = file.name;
+    if (s.length > 15) {
+        s = s.substring(0, 12) + "...";
+    }
+    if (isDeleting) {
+        temp.classList.remove("HoverClassRed", "HoverClassGreen");
+        temp.classList.add('HoverClassRed')
+    } else {
+        temp.classList.remove("HoverClassRed", "HoverClassGreen");
+        temp.classList.add('HoverClassGreen')
+    }
+    const inside = document.createElement('a');
+    inside.appendChild(document.createTextNode(s)); // remove ".pdf"
+
+    temp.appendChild(inside);
+    fileList.appendChild(temp);
+}
+
+function setDownloadOrDelete() {
+    const d = document.getElementsByClassName("dd")[0];
+
     d.classList.remove("deleteOrDownloadRed", "deleteOrDownloadGreen");
     d.classList.remove("HoverClassRed", "HoverClassGreen");
-    const text = document.createElement('div');
-    text.className = "dd-text";
+    const text = document.getElementsByClassName('dd-text')[0];
 
+    removeAllChildNodes(text)
     if (isDeleting) {
-        d.className = "deleteOrDownloadRed";
-        d.classList.add('HoverClassRed');
+        d.classList.add("deleteOrDownloadRed", 'HoverClassRed');
         text.appendChild(document.createTextNode("Delete"))
         text.style.fontSize = convertHeight(1.3)
-        const uploadContainers = document.getElementsByClassName("upload-container");
-        for (let i = 0; i < uploadContainers.length; i++) {
-            uploadContainers[i].classList.remove("HoverClassRed", "HoverClassGreen");
-            uploadContainers[i].classList.add('HoverClassRed')
-        }
     } else {
-        d.className = "deleteOrDownloadGreen";
-        d.classList.add('HoverClassGreen');
+        d.classList.add("deleteOrDownloadGreen", 'HoverClassGreen');
         text.appendChild(document.createTextNode("Download"))
         text.style.fontSize = convertHeight(1.15)
-        const uploadContainers = document.getElementsByClassName("upload-container");
-        for (let i = 0; i < uploadContainers.length; i++) {
-            uploadContainers[i].classList.remove("HoverClassRed", "HoverClassGreen");
-            uploadContainers[i].classList.add('HoverClassGreen')
-        }
     }
-    d.appendChild(text);
+}
 
-
-
-
-
+function setUpload() {
+    const u = document.getElementsByClassName("uu")[0];
+    const text = document.getElementsByClassName('uu-text')[0];
+    removeAllChildNodes(text)
+    text.appendChild(document.createTextNode("Upload"))
+    text.style.fontSize = convertHeight(1.3)
+    u.style.fontSize = convertHeight(1.3)
+    const upload = document.getElementById("uploadInput");
+    upload.addEventListener('change', async function () {
+        for (let i = 0; i < upload.files.length; i++) {
+            const name = "files/" + upload.files[i].name
+            await storageRef.child(name).put(upload.files[i])
+            await getSingleUploadedFile(name)
+        }
+    })
 }
 
 async function getUploadedFiles() {
-    const list = [];
-
+    uploadedFileList.splice(0, uploadedFileList.length);
     const a = await storageRef.child("files").list();
     for (let i = 0; i < a.items.length; i++) {
         const path = a.items[i].location.path_;
-        list.push(new URLAndName(await storageRef.child(path).getDownloadURL(), path.substring(6)));
+        uploadedFileList.push(new URLAndName(await storageRef.child(path).getDownloadURL(), path.substring(6)));
+        setupFileList();
     }
-    return list;
 }
+
+async function getSingleUploadedFile(name) {
+    const a = new URLAndName(await storageRef.child(name).getDownloadURL(), name.substring(6))
+    for (let i = 0; i < uploadedFileList.length; i++) {
+        if (a.equals(uploadedFileList[i])) {
+            console.log("repeat")
+            return;
+        }
+    }
+    uploadedFileList.push(a);
+    AddSingleFileToList(a);
+}
+
 
 function deleteOrDownloadFile(file) {
     if (isDeleting) {
@@ -132,12 +158,11 @@ function deleteOrDownloadFile(file) {
 
 async function uploadFiles(files) {
     for (let i = 0; i < files.length; i++) {
-        await storageRef.child("files/" + files[i].name).put(files[i]).on('state_changed', function (snapshot) {
-            // Observe state change events such as progress, pause, and resume
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log(`File #${i} is ${Math.floor(progress)}% done`);
-        })
+        await storageRef.child("files/" + files[i].name).put(files[i])
+        // .on('state_changed', function (snapshot) {
+        //     const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //     console.log(`File #${i} is ${Math.floor(progress)}% done`);
+        // })
     }
 }
 
@@ -155,23 +180,13 @@ function openFromUrl(url) {
 };
 
 function deleteFile(name) {
-    for (let i = 0; i < uploadFilesList.length; i++) {
-        if (name.localeCompare(uploadFilesList[i].name) == 0){
-            uploadFilesList.splice(i, 1);
+    for (let i = 0; i < uploadedFileList.length; i++) {
+        if (name.localeCompare(uploadedFileList[i].name) == 0) {
+            uploadedFileList.splice(i, 1);
             break;
         }
     }
 
     storageRef.child("files").child(name).delete()
-    const f = document.getElementsByClassName("uploadedFiles")[0];
-    setupFileList(f);
-    const uploadContainers = document.getElementsByClassName("upload-container");
-    for (let i = 0; i < uploadContainers.length; i++) {
-        uploadContainers[i].classList.remove("HoverClassRed", "HoverClassGreen");
-        if (isDeleting) {
-            uploadContainers[i].classList.add('HoverClassRed')
-        } else {
-            uploadContainers[i].classList.add('HoverClassGreen');
-        }
-    }
+    setupFileList();
 }
